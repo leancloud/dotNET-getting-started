cb707d52f059e9529ac16e8c4428f72e4e60d70c
WuJun <wujun19890209@gmail.com>
2018-07-06 00:01:50 +0800

    fix: register subclass.

--- web/Program.cs
+++ web/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using LeanCloud;
+using LeanCloud.Core.Internal;
 using LeanCloud.Engine;
+using LeanCloud.Storage.Internal;
+
 namespace web
 {
     class Program
     {
         static void Main(string[] args)
         {
+            AVClient.Configuration configuration = new AVClient.Configuration()
+            {
+                ApplicationId = "315XFAYyIGPbd98vHPCBnLre-9Nh9j0Va",
+                ApplicationKey = "Y04sM6TzhMSBmCMkwfI3FpHc",
+                MasterKey = "Ep3IHWFqi41DMm44T49lKy07"
+            };
+
+            AVClient.Initialize(configuration);
+
             Cloud cloud = new Cloud().SetHooks().UseLog();
             cloud.Start(args);
         }
