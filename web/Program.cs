1589be87044086709c172afd022763994f1b2eb7
WuJun <wujun19890209@gmail.com>
2018-07-06 12:25:42 +0800

    feat: remove AVClient init.

--- web/Program.cs
+++ web/Program.cs
@@ -12,22 +12,6 @@ namespace web
     {
         static void Main(string[] args)
         {
-            AVClient.Configuration configuration = new AVClient.Configuration()
-            {
-                ApplicationId = "315XFAYyIGPbd98vHPCBnLre-9Nh9j0Va",
-                ApplicationKey = "Y04sM6TzhMSBmCMkwfI3FpHc",
-                MasterKey = "Ep3IHWFqi41DMm44T49lKy07"
-            };
-            var netEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var leanEnv = Environment.GetEnvironmentVariable("LEANCLOUD_APP_PROD");
-            var isLeanProd = "1".Equals(leanEnv);
-            var isNetProd = EnvironmentName.Production.Equals(netEnv);
-
-            Console.WriteLine($"leanEnv:{leanEnv};isLeanProd:{isLeanProd};netEnv:{netEnv};isNetProd:{isNetProd};isProd:{isLeanProd || isNetProd}");
-
-
-            AVClient.Initialize(configuration);
-
             Cloud cloud = new Cloud().SetHooks().UseLog();
             cloud.Start(args);
         }
