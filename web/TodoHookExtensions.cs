78201d375fb782edb5de1f0932b8f66c2671e6f6
WuJun <wujun19890209@gmail.com>
2018-07-09 17:24:47 +0800

    feat: try before update.

--- web/TodoHookExtensions.cs
+++ web/TodoHookExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
+using LeanCloud;
+using LeanCloud.Core.Internal;
 using LeanCloud.Engine;
 
 namespace web
@@ -16,7 +18,28 @@ namespace web
                     if (title.Length > 20) todo["title"] = title.Substring(0, 20);
                 // returning any value will be ok.
                 return Task.FromResult(true);
-            });
+            }).BeforeSave("Review", review =>
+            {
+                var comment = review.Get<string>("comment");
+                if (comment.Length > 140) review["comment"] = comment.Substring(0, 137) + "...";
+                return Task.FromResult(comment);
+            }).AfterSave("Review", async review =>
+            {
+                var post = review.Get<AVObject>("post");
+                await post.FetchAsync();
+                post.Increment("comments");
+                await post.SaveAsync();
+            }).AfterSave("", async user =>
+            {
+                if (user is AVUser avUser)
+                {
+                    avUser.Set("from", "LeanCloud");
+                    await avUser.SaveAsync();
+                }
+            }).BeforeUpdate("Todo", (EngineObjectHookContext context) =>
+             {
+                 return Task.FromResult("haha");
+             });
             return cloud;
         }
     }
